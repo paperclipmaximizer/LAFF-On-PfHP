c966969f70d434366cbead9f8514070d39fb2b70
liam <50953884+paperclipmaximizer@users.noreply.github.com>
2023-11-26 17:44:35 +1100

    axpy & gemv_axpi

--- Assignments/Week1/C/Gemv_J_Axpy.c
+++ Assignments/Week1/C/Gemv_J_Axpy.c
@@ -2,11 +2,16 @@
 #define chi( i )  x[ (i)*incx ]         // map chi( i )  to array x
 #define psi( i )  y[ (i)*incy ]         // map psi( i )  to array y
 
+// axpy takes n, alpha, *x, incx, *y, incy
 void Axpy( int, double, double *, int, double *, int );
 
 void MyGemv( int m, int n, double *A, int ldA,
            double *x, int incx, double *y, int incy )
 {
+  // here we are looping over the columns of A
+  // 
   for ( int j=0; j<n; j++ )
-    Axpy(    ,      ,        ,    ,    ,       );
+    // m: we are incrementing over m instead of n
+    // 
+    Axpy( m, chi( j ), &alpha (0,j) , 1, y, incy );
 }
